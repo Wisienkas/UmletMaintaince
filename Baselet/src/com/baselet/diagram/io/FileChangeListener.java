079e325cadf3d995d840c4f058857b360457bb47
almeh12@student.sdu.dk <almeh12@student.sdu.dk>
2014-11-28 17:13:45 +0100

    FileChangeListener is now started and stopped correctly when diagrams are opened, saved to new files and closed.
    Remaining is to do the merge as an action that can be undone and redone.

--- Baselet/src/com/baselet/diagram/io/FileChangeListener.java
+++ Baselet/src/com/baselet/diagram/io/FileChangeListener.java
@@ -5,12 +5,14 @@ import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Paths;
 import java.nio.file.WatchEvent;
 import java.nio.file.WatchKey;
 import java.nio.file.WatchService;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -34,6 +36,10 @@ public class FileChangeListener extends Thread {
 	boolean dontShow;
 	int selectedOption;
 
+	private WatchService watcher;
+	private Scanner openFileScanner;
+	private InputStream resultInputStream;
+
 	public FileChangeListener(DiagramHandler handler, DiagramFileHandler fileHandler, File file) {
 		this.openFile = file;
 		this.diagramHandler = handler;
@@ -54,14 +60,16 @@ public class FileChangeListener extends Thread {
 	public void run() {
 		try {
 			java.nio.file.Path myDir = Paths.get(openFile.getParent());
-			WatchService watcher = myDir.getFileSystem().newWatchService();
+			watcher = myDir.getFileSystem().newWatchService();
 			myDir.register(watcher, ENTRY_MODIFY);
 
 			log.info("Now watching file: " + openFile.getAbsolutePath());
 
 			WatchKey watchKey = watcher.take();
-			while (watchKey != null) {
-				for (WatchEvent<?> event : watchKey.pollEvents()) {
+			while (watchKey != null && !Thread.currentThread().isInterrupted()) {
+				List<WatchEvent<?>> pollEvents = watchKey.pollEvents();
+				for (int i = 0; i < pollEvents.size(); i++) {
+
 					if (openFile.lastModified() == lastModified) {
 						continue;
 					}
@@ -69,11 +77,13 @@ public class FileChangeListener extends Thread {
 					// Sleep for a short period to ensure the file operation is finished when we start reading
 					Thread.sleep(2000);
 
-					String othersChanges = new Scanner(openFile).useDelimiter("\\Z").next();
+					openFileScanner = new Scanner(openFile);
+					String othersChanges = openFileScanner.useDelimiter("\\Z").next();
+					openFileScanner.close();
 					String myChanges = diagramFileHandler.createStringToBeSaved();
 					lastModified = openFile.lastModified();
 
-					//If we were the ones doing the change or the user does not want to merge in changes then just continue
+					// If we were the ones doing the change or the user does not want to merge in changes then just continue
 					if (othersChanges.trim().equals(myChanges.trim()) || !showYesNOOptionPane()) {
 						continue;
 					}
@@ -84,15 +94,16 @@ public class FileChangeListener extends Thread {
 
 					originalFileContent = othersChanges;
 
+					//Now update the diagram onscreen with the new merged result
 					synchronized (diagramHandler.getDrawPanel().getGridElements()) {
 						diagramHandler.getDrawPanel().getGridElements().clear();
 						diagramHandler.getDrawPanel().removeAll();
 
 						SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
-						InputStream input = new ByteArrayInputStream(mergedResult.getBytes());
+						resultInputStream = new ByteArrayInputStream(mergedResult.getBytes());
 						InputHandler xmlhandler = new InputHandler(diagramHandler);
-						parser.parse(input, xmlhandler);
-						input.close();
+						parser.parse(resultInputStream, xmlhandler);
+						resultInputStream.close();
 
 						diagramHandler.getDrawPanel().updatePanelAndScrollbars();
 					}
@@ -100,10 +111,29 @@ public class FileChangeListener extends Thread {
 				watchKey.reset();
 				watchKey = watcher.take();
 			}
+		} catch (InterruptedException e) {
+			// The thread is interrupted meaning the diagram has been closed. We will therefore just stop the FileChangedListener
+			log.info("Stopping FileChangeListener for file " + openFile.getAbsolutePath());
+			Thread.currentThread().interrupt();
 		} catch (Exception e) {
+			// If any other exception apart from an interrupted exception occurs, try restarting the FileChangeListener
 			e.printStackTrace();
-			System.out.println("Error: " + e.toString());
 			run();
+		} finally {
+			//Making sure all resources are closed
+			try {
+				if (watcher != null) {
+					watcher.close();
+				}
+				if (openFileScanner != null) {
+					openFileScanner.close();
+				}
+				if(resultInputStream != null){
+					resultInputStream.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
@@ -120,7 +150,6 @@ public class FileChangeListener extends Thread {
 	}
 
 	public void stopListening() {
-		// TODO Auto-generated method stub
-		
+		this.interrupt();
 	}
 }
