3c23d52ce7c3fb5897828da3ce7b2a4350aae109
Nikolaj Reibke <nikolajsr12@gmail.com>
2014-12-01 03:47:15 +0100

    added all test classes to test suite

--- Baselet/test/com/baselet/control/AllTests.java
+++ Baselet/test/com/baselet/control/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({TestRelate.class, TestRelateClass.class, TestRelateManager.class})
 public class AllTests {
 
 }
