defe9ac1733ca889889fdb03e5f3b0966ec7e474
Henrik Lindberg Larsen <heinzdmx@gmail.com>
2014-12-02 11:07:17 +0100

    Clean up tests

--- Baselet/test/com/umlet/language/ClassCodeConverterTestWhiteBox.java
+++ Baselet/test/com/umlet/language/ClassCodeConverterTestWhiteBox.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class ClassCodeConverterTestWhiteBox extends ClassCodeConverterTestBase {
 
 	@Test
-	public void fail() throws FileNotFoundException
+	public void failWrongSectionSplit() throws FileNotFoundException
 	{
 		File file = baseTest("ClassFail1\n--\n-asd", "ClassFail1.java", false);
 		
@@ -19,9 +19,9 @@ public class ClassCodeConverterTestWhiteBox extends ClassCodeConverterTestBase {
 	}
 
 	@Test
-	public void fail2() throws FileNotFoundException
+	public void failWrongSectionPlacement() throws FileNotFoundException
 	{
-		File file = baseTest("/ClassFail2/\n--\n-morning : String\n--+morning()", "ClassFail2.java", false);
+		File file = baseTest("/ClassFail2/\n--\n-morning : String\n+morning()\n--\n", "ClassFail2.java", false);
 		
 		Assert.assertFalse(file.exists());
 		
@@ -38,7 +38,7 @@ public class ClassCodeConverterTestWhiteBox extends ClassCodeConverterTestBase {
 	}
 	
 	@Test
-	public void testInterfaces2() throws FileNotFoundException
+	public void testClassWithInterface() throws FileNotFoundException
 	{
 		String attributes = "<<Simple>>\ncom.umlet.test::Class1\n";
 		String filename = "com/umlet/test/Class1.java";
@@ -55,53 +55,10 @@ public class ClassCodeConverterTestWhiteBox extends ClassCodeConverterTestBase {
 		file.delete();
 	}
 	
-	@Test
-	public void testClass1() throws FileNotFoundException
-	{
-		String attributes = "Classtest\n";
-		String filename = "Classtest.java";
-		File file = baseTest(attributes, filename);
-		file.delete();
-	}
-
-	@Test
-	public void testClass2() throws FileNotFoundException
-	{
-		String attributes = "<<Interface>>\n" + "com.umlet.test.unittest.Class2";
-		String filename = "com/umlet/test/unittest/Class2.java";
-		File file = baseTest(attributes, filename);
-		file.delete();
-	}
-
-	@Test
-	public void testClass3() throws FileNotFoundException
-	{
-		String attributes = "<<Interface>>\n" + "com.umlet.test.unittest.Class3" + "\n--\n--\n+morning()\n";
-		String filename = "com/umlet/test/unittest/Class3.java";
-		File file = baseTest(attributes, filename);
-
-		file.delete();
-	}
-
-	@Test
-	public void testClass4() throws FileNotFoundException
-	{
-		String attributes = "/" + "com.umlet.test.unittest::Class4" + "/\n--\n-morning : String\n--\n+morning()\n";
-		String filename = "com/umlet/test/unittest/Class4.java";
-		File file = baseTest(attributes, filename);
-
-		file.delete();
-	}
-
-	@Test
-	public void testClass5() throws FileNotFoundException
-	{
-		String attributes = "//hej med dig\n/" + "com.umlet.test.unittest::Class5" + "/\n--\n-moring : String\n--\n+method()\n";
-		String filename = "com/umlet/test/unittest/Class5.java";
-		File file = baseTest(attributes, filename);
-		file.delete();
-	}
-
+	/**
+	 * Test content based on a diagram created from code
+	 * @throws FileNotFoundException
+	 */
 	@Test
 	public void generatedTest() throws FileNotFoundException
 	{
